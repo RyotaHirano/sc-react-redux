7d2b113b1fbc84774d6ad3068ac3483f142ac499
RyotaHirano <hirano@tsumikiinc.com>
2016-06-21 12:08:42 +0900

    add
    get soundcloudDatum.

--- src/components/Stream/presenter.js
+++ src/components/Stream/presenter.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-function Stream({ tracks = [] }) {
+function Stream({ user, tracks = [], onAuth }) {
   return (
     <div>
-      {
-        tracks.map((track, key) => {
-          return <div className="track" key={key}>{track.title}</div>;
-        })
-      }
+      <div>
+        {
+          user ?
+            <div>{user.username}</div> :
+            <button onClick={onAuth} type="button">Login</button>
+        }
+      </div>
+      <div>
+        {
+          tracks.map((track, key) => {
+            return <div className="track" key={key}>{track.title}</div>;
+          })
+        }
+      </div>
     </div>
   );
 }
