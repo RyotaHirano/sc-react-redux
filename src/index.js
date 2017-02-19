f523bca5f506c89cdf0182eb3afcefb45f76206a
Ryota Hirano <hirano@tsumikiinc.com>
2017-02-19 23:13:39 +0900

    fix dir stores

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/configureStore';
+import configureStore from './stores/configureStore';
 import * as actions from './actions';
 import Stream from './components/Stream';
 
