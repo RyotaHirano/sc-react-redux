9e64c01d63d2beed4a571f2b9d661e7317c6580d
Ryota Hirano <hirano@tsumikiinc.com>
2017-02-22 08:58:34 +0900

    add bindActionCreators to Stream component

--- src/components/Stream/index.js
+++ src/components/Stream/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import * as actions from '../../actions';
 import Stream from './presenter';
 
 function mapStateToProps(state) {
@@ -9,4 +11,10 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(Stream);
+function mapDispatchProps(dispatch) {
+  return {
+    onAuth: bindActionCreators(actions.auth, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchProps)(Stream);
