8a9af8a56d75a83b0a25c7cdfd75bbf1a2f949b1
Ryota Hirano <hirano@tsumikiinc.com>
2017-02-24 09:11:37 +0900

    add redux-thunk to configureStore

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { browserHistory } from 'react-router';
 import { routerMiddleWare } from 'react-router-redux';
 import rootReducer from '../reducers/index';
@@ -7,7 +8,7 @@ import rootReducer from '../reducers/index';
 const logger = createLogger();
 const router = routerMiddleWare(browserHistory);
 
-const createStoreWithMiddleWare = applyMiddleware(router, logger)(createStore);
+const createStoreWithMiddleWare = applyMiddleware(thunk, router, logger)(createStore);
 
 export default function configureStore(initialState) {
   return createStoreWithMiddleWare(rootReducer, initialState);
